b421ff3437b68697bdcd88b23d45b6984efc52ce
Stefan Annell <steann@kth.se>
2022-12-03 12:13:35 +0100

    Added in a gui mode, fixed a few bugs with generation that could fail and circular dependencies and updated most presents.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import smtplib, ssl, argparse, random
 from getpass import getpass
-from message import GetMessage
+from message import GetMessage, GetWishlist
+import re
 
 class Participant:
     def __init__(self, name, email, wishList=None):
@@ -9,7 +10,7 @@ class Participant:
         self.WishList = wishList
     
     def __str__(self):
-        return "Participant Name={}, Email={}, Wishlist={}".format(self.Name, self.Email, self.WishList)
+        return "Participant Name={}\nEmail={}\n{}".format(self.Name, self.Email, GetWishlist(self))
 
     Name = ""
     Email = ""
@@ -19,43 +20,39 @@ participantsDefinition = [
     Participant("Stefan Annell",
             "steann@kth.se",
             [
-            "Några snygga klätter byxor, t ex: https://www.addnature.com/black-diamond-notion-pants-herr-M709782.html?vgid=G1265652&cgid=236014",
-            "Någon snygg patagonia tröja eller t-short, storlek M",
-            "Boulder kritpåse, t ex: https://www.addnature.com/petzl-sakab-chalk-bag-M917323.html",
-            "Svarta strumpor, storlek 44 (brukar köpa från brothers)"
+                "Klättertränings redskap, https://www.addnature.com/max-climbing-maxgrip-hybrid-M889083.html#cgid=236483",
+                "Quickdraws, https://www.addnature.com/black-diamond-hotforge-quickpack-12cm-M841006.html?vgid=G1182940#cgid=236464",
+                "Kettlebel ~24-28 kg (obs! Beställ hem till oss istället för upp till Norrland så slipper ta på flyg!) https://gymkompaniet.se/kettlebells-neopren-8-48kg-recoil?___store=default",
+                "1x Låskarbin, https://www.addnature.com/edelrid-hms-strike-slider-fg-ii-M1152029.html?vgid=G1703525#cgid=236466",
+                "2x Standard yoga block, gärna kork vilket märke som helst funkar.",
         ]),
     Participant("Anna-Karin Ek",
             "annakarin.ek@gmail.com",
             [
-            "FLEECEJACKA Sarek Camp Jacket mörkgrön (storlek SMALL) 999 kr. KÖP FRÅN DENNA BUTIK, så kan jag byta ut den om storleken ej är bra: https://www.grandpastore.se/sv/sarek-camp-jacket-ws-field-green",
-            "MASSAGE full body massage, 1045 kr (måndag-torsdag): https://elementsspa.co/stockholm/behandling/massage/",
-            "BEIGE Wool overshirt, Storlek SMALL, 1245 kr: https://www.arket.com/en_sek/women/shirts-blouses/product.wool-overshirt-beige.0932372003.html?gclid=CjwKCAiAhreNBhAYEiwAFGGKPM_enzvnloDccJosD_3uLV6lsSAENXTFAU9oz9dgE6Bbd01yAqB7TRoC23wQAvD_BwE&fbclid=IwAR2lu9JcYiaPggfPM_C18eaK5MC9DrF1P8Ftd0LGBJfpFvvHpDHp9Y9EwQw"
+                "Halsband från Bluebillie, STOR SOL GULD: https://bluebillie.com/sv/shop/favourite-combinations-sv/big-sun-gold-on-plain-chain-gold/. De saker Anna har listat är dyrare än gränsen på 1000 kr, Anna kommer givetvis betala mellanskillnaden till den som köper till henne.",
+                "Fleecetröja från Astrid Wild, färg SVART, storlek SMALL: https://www.astridwild.com/sv/collections/fleeces-tops/products/minna-wool-fleece-jacket?variant=42485110571232 (OBS spara retursedeln + paketet om du köper tröjan utifall jag behöver byta storlek. OBS på deras hemsida kan du lägga till månadens gåva med köpet av fleecetröjan, gör gärna det i samband med köpet!). De saker Anna har listat är dyrare än gränsen på 1000 kr, Anna kommer givetvis betala mellanskillnaden till den som köper till henne.",
         ]),
 
     Participant("Boel Ek",
             "boel.ek@live.se",
             [
-            "En ny putter. Mats kan fixa detta via vår pro som bor här på Bergnäset.",
-            "Presentkort på resturang CG:s",
-            "Tidningspremunation på t ex Årets Runt och Allers"
+            "Obs special! Mats köpte ett par tofflor från Naturkompaniet till Boel, swisha honom 1000kr - årspremunation på Allers eller Året runt.",
         ]),
 
     Participant("Mats Ek",
             "mats.ek@hotmail.se",
             [
-            "Varma rejäla vinterhandskar (typ skidhandskar)",
-            "Underställs långkalsonger (typ aclima)",
-            "Coola och bra solglasögon (typ Gustavs med något specialglas så att naturen blir grönare)"
+            "https://www.jackjones.com/sv-se/product/12190170_6/mock-neck-stickad-troeja stl Large",
+            "https://www.intersport.se/klader/underklader/understall/mckinley-dylan-ux-understallsbyxor-herr/black-night stl Large",
         ]),
 
     Participant("Gustav Ek",
             "gustav.ek86@gmail.com",
             [
-            "En bra kött-termometer",
-            "Ansiktskräm",
-            "Någonting roligt från typ Cervera, kök/inredning",
-            "Någon rolig t-shirt storlek M",
-            "Skoterkänga Polyver Winter"
+                "Iitala Essence dricksglas 35 cl",
+                "Le creuset Ugnsform med lock. Gärna orange färg.",
+                "Skeppshult Gjutjärnspanna med trähandtag .",
+                "Prydnader/inredning kanske vasmed tillhörande kvistar/grenar eller dylikt, snygg väggklocka"
         ]),
 
     Participant("Jacob Ek",
@@ -124,23 +121,45 @@ def FindEmailServer(sender_email):
             return emailServer
     return None
 
+def ValidateTargets(participants):
+    """ Circular dependency check"""
+    def findParticipant(participants, participantGiver):
+        for participant in participants:
+            if participant.Giver == participantGiver:
+                return participant
+        return None
+
+    for participant in participants:
+        targetParticipant = findParticipant(participants, participant.Target)
+        if participant.Giver == targetParticipant.Target:
+            if targetParticipant.Giver == participant.Target:
+                #print("---")
+                #print("Circular dependency!")
+                #print(targetParticipant.Giver.Name, "<->", targetParticipant.Target.Name)
+                #print("---")
+                return False
+                
+    """ Self check, cannot give yourself a present """
+    for participant in participants:
+        if participant.Giver == participant.Target:
+            #print("---")
+            #print("Self check failed!")
+            #print(participant.Giver.Name, "<->", participant.Target.Name)
+            #print("---")
+            return False
+    
+    return True 
+
 def GeneratePresentTargets(participants):
     presentTargets = []
-    presentTargets = participants.copy()
-    random.shuffle(presentTargets)
+    participantsTargets = participants.copy()
+    random.shuffle(participantsTargets)
     
     while participants:
-        participant = participants.pop()
-        foundTarget = False
-        for presentTarget in presentTargets:
-            if participant != presentTarget:
-                foundTarget = True
-                presentTargets.append(PresentTarget(participant, presentTarget))
-                presentTargets.remove(presentTarget)
-                break;
+        giver = participants.pop()
+        target = participantsTargets.pop()
+        presentTargets.append(PresentTarget(giver, target))
 
-        if not foundTarget:
-            return None
     return presentTargets
 
 def GetPresentTargetsList():
@@ -150,12 +169,25 @@ def GetPresentTargetsList():
     while not presentTargets:
         n += 1
         print("Generation nr: {}".format(n))
-        presentTargets = GeneratePresentTargets(participantsDefinition)
-        if n >= 10:
+        presentTargets = GeneratePresentTargets(participantsDefinition.copy())
+        if not ValidateTargets(presentTargets):
+            presentTargets = None
+
+        if n >= 100:
             print("Generation failed!")
             return None
     return presentTargets
 
+ 
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+ 
+def check(email):
+    result = re.fullmatch(regex, email)
+    if(result):
+        print("Valid Email")
+    else:
+        print("Invalid Email")
+    return result
 
 def main(sender_email, password, test):
     presentTargets = GetPresentTargetsList()
@@ -168,8 +200,8 @@ def main(sender_email, password, test):
 
     if not sender_email:
         sender_email = input("Type your email and press enter: ")
-    if "@" not in sender_email:
-        print("Not a valid email address")
+    if not check(sender_email):
+        return
     if not password:
         password = getpass()
 
@@ -180,10 +212,51 @@ def main(sender_email, password, test):
     else:
         print("Email server not defined in EmailServers, cannot send away email.")
 
+    
+
+import customtkinter
+def gui(email="Email", password=""):
+    customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    app.geometry("1024x900")
+    app.title("Christmas Present Game")
+
+
+    entry_1 = customtkinter.CTkEntry(master=app, placeholder_text=email)
+    entry_1.pack(pady = 12, padx = 10)
+
+    entry_2 = customtkinter.CTkEntry(master=app, placeholder_text=password, show="*")
+    entry_2.pack(pady = 12, padx = 10)
+
+
+    def send():
+        main(entry_1.get(), entry_2.get(), True)
+    button = customtkinter.CTkButton(master=app, text="Send", command=send)
+    button.pack(pady = 12, padx = 10)
+
+    def test():
+        main(entry_1.get(), entry_2.get(), True)
+    button2 = customtkinter.CTkButton(master=app, text="Test", command=test)
+    button2.pack(pady = 12, padx = 10)
+
+    participantsButtons = []
+    for participant in participantsDefinition:
+        outputText = str(participant)
+        text = customtkinter.CTkLabel(master=app, text=outputText, anchor="w", width=250)
+        text.pack(pady = 5, padx = 5)
+
+    app.mainloop()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--gui", help="Open a GUI of the software.", action="store_true")
     parser.add_argument("-e", "--email", help="The address that will send off the email's.")
     parser.add_argument("-p", "--password", help="Password for the sender email (can be provided as hidden input as well).")
     parser.add_argument("-t", "--test", help="Test of generatior mode.", action="store_true")
     args = parser.parse_args()
-    main(args.email, args.password, args.test)
+    if args.gui:
+        gui(args.email, args.password)
+    else:
+        main(args.email, args.password, args.test)
